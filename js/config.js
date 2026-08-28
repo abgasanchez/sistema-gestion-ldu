a27b78960eb2ebd663227980c11f1ef495bc5c4d
abgasanchez <abg.asanchez@gmail.com>
2026-08-28 12:06:45 -0500

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-window.LDU_CONFIG = {
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbw2QZ1XXfb_35iq-TemdHgoE08QE4S3lVe3GT4a7PubcJH598Egr2gZGoug9DvZe5epXg/exec',
-  DEMO_MODE: false,
-  APP_NAME: 'Sistema de Gestión LDU',
-  AUTH_ENABLED: true,
-  AUTH_BACKEND_REQUIRED: true
-};
