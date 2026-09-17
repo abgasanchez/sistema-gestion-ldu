8be2a2dc81f426476025b5ee6fe637d9a22194b6
abgasanchez <abg.asanchez@gmail.com>
2026-09-17 15:31:18 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window.LDU_CONFIG = {
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbxKHCbGMZP365p7kjHvWStBjw_QFiLjJ8xrc0QR-9ZcNa0sBn8npt1vVewO6lqaWdmJyg/exec',
+  DEMO_MODE: false,
+  APP_NAME: 'Sistema de Gestión LDU',
+  AUTH_ENABLED: true,
+  AUTH_BACKEND_REQUIRED: true
+};
